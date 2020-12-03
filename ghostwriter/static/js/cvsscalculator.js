6a341ac67a5235de22d162e4761e975cf3f1bcfc
Laura Pardo <lpardo@redhat.com>
2020-12-03 23:39:12 +0100

    fix severity values

--- ghostwriter/static/js/cvsscalculator.js
+++ ghostwriter/static/js/cvsscalculator.js
@@ -1,5 +1,12 @@
 $(function() {
-  
+
+    var severities = {
+      low: 3.9,
+      medium: 6.9,
+      high: 8.9,
+      critical: 10.0
+    }
+
     //calculate the cvss score - vector - severity
     var attackVector = {}
     $(".cvss-input").click(function(){
@@ -22,6 +29,31 @@ $(function() {
           }
         })
     })
+
+    if ($("#id_cvss_score").val() != null){
+        fillCalculator()
+    }
+
+    function calculateSeverity(s){
+        var severity
+        $.each(severities, function(k,v){
+            if (s <= v){
+                severity = k
+                return false
+            }
+        })
+        return severity
+    }
+
+    function fillCalculator(){
+        var severity = calculateSeverity(parseFloat($("#id_cvss_score").val()))
+        $(".scoreRating").addClass(severity)
+        $("#baseMetricScore").text($("#id_cvss_score").val())
+        $("#baseSeverity").text(severity)
+    }
+
+
+
 
     //check the buttons in the cvss calculator
     $.each($("#id_cvss_vector").val().split("/"), function(i,v){
